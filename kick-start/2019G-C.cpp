e333c69fda56eea2381ca76b5e8a020cc07e5378
upupming <upupming@gmail.com>
2019-10-22 11:29:53 +0800

    Finished: 1012

--- kick-start/2019G-C.cpp
+++ kick-start/2019G-C.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 #include <algorithm>
-#include <cmath>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
 int t, n, h, a[50], b[50];
 
 struct Pair {
-    // pa, pb 分别表示 A, B 得到的 happiness
+    // pa, pb for happiness of A(Aninda), B(Boon-Nam)
     long long pa, pb;
     Pair(const Pair & p) {
         this->pa = p.pa;
@@ -47,20 +45,17 @@ long long solve() {
 
     vector<Pair> pairsA, pairsB;
     pointPair.pa = pointPair.pb = 0;
-    // 关键思想在折半
+    // divide into 2 sets
     dfs(0, n / 2, pointPair, pairsA);
     dfs(n / 2, n, pointPair, pairsB);
 
     sort(pairsA.begin(), pairsA.end(), cmp);
     sort(pairsB.begin(), pairsB.end(), cmp);
 
-    // O(3^{N/2})
+    // for all pairs: O(3^{N/2})
     for (vector<Pair>::iterator it = pairsA.begin(); it != pairsA.end(); it++) {
-        // cout << "(h1, h2) = (" << (it->pa) << ", " << (it->pb) << ")" << endl;
-        // // 不能用 set，因为 set 不计算重复元素
-        // priority_queue<long long> s;
         long long i = -1, low = 0, high = pairsB.size() - 1;
-        // 这里必须使用二分查找
+        // binary search: log(3^{N/2})
         while(low <= high) {
             long long mid = (low + high) / 2;
             if (pairsB[mid].pa < h - it->pa) {
@@ -70,67 +65,20 @@ long long solve() {
                 high = mid - 1;
             }
         }
-        // cout << "i = " << i << endl;
-
-        // O(3^{N/2})
-        // for (i = 0; i < pairsB.size() && !(pairsB[i].pa >= h - it->pa); i++) ;
-
         for (; i < pairsB.size() && i >= 0; i++) {
-            // cout << "(p1, p2) = (" << (pairsB[i].pa) << ", " << (pairsB[i].pb) << ")" << endl;
-            // s.push(pairsB[i].pb);
             if (pairsB[i].pb >= (h - it->pb)) res++;
-        }
-        // cout << "distance(s.lower_bound(" << h - it->pb <<"), s.end()) = " << distance(s.lower_bound(h - it->pb), s.end()) << endl; 
-        // while(!s.empty() && s.top() >= (h - it->pb)) {
-        //     res++;
-        //     s.pop();
-        // }
-        // res += distance(s.lower_bound(h - it->pb), s.end());
-    }
-    return res;
-}
-
-long long solve_old() {
-    // return getChoices(a) * getChoices(b);
-    long long maxVal = pow(2, n), res = 0;
-    for (long long i = 0; i < maxVal; i++) {
-        for (long long j = 0; j < maxVal; j++) {
-            if ((i|j) != maxVal-1) continue;
-            // cout << "i = " << i << ", j = " << j << endl;
-            long long suma = 0, sumb = 0;
-            for (int k = 0; k < n; k++) {
-                int lastBit = (i >> k) & 1;
-                suma += lastBit ? a[k] : 0;
-                lastBit = (j >> k) & 1;
-                sumb += lastBit ? b[k] : 0;
-            }
-            // cout << "suma = " << suma << ", sumb = " << sumb << endl;
-            if (suma >= h && sumb >= h) res += 1;
         }
     }
     return res;
 }
 
 int main() {
-    cin >> t;
+    scanf("%d", &t);
     for (int i = 1; i <= t; i++) {
-        cin >> n >> h;
-        for (int i = 0; i < n; i++) cin >> a[i];
-        for (int i = 0; i < n; i++) cin >> b[i];
+        scanf("%d%d", &n, &h);
+        for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+        for (int i = 0; i < n; i++) scanf("%d", &b[i]);
         printf("Case #%d: %lld\n", i, solve());
     }
     return 0;
 }
-
-/**
-
-1
-1 0
-3
-2
-
-1
-2 0
-3 3
-2 4
-*/
---
+++ upupming/1012.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+struct Student {
+    string id;
+    int c, m, e, a;
+} student;
+
+vector<Student> students;
+int n, m;
+string query;
+map<string, int> rnk;
+map<string, char> rnkBy;
+
+int cmp0(const Student& a, const Student& b) { return a.a > b.a; }
+int cmp1(const Student& a, const Student& b) { return a.c > b.c; }
+int cmp2(const Student& a, const Student& b) { return a.m > b.m; }
+int cmp3(const Student& a, const Student& b) { return a.e > b.e; }
+typedef int (*cmpType) (const Student&, const Student&);
+
+cmpType cmpAll[] = { cmp0, cmp1, cmp2, cmp3 };
+char by[] = "ACME";
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> student.id >> student.c >> student.m >> student.e;
+        // 四舍五入
+        student.a = (student.c + student.m + student.e) / 3.0 + 0.5;
+        students.push_back(student);
+    }
+
+    // 注意多人排名相等的情况！！！
+
+    sort(students.begin(), students.end(), cmp0);
+    for (int i = 0; i < n; i++) {
+        int tmp = i;
+        while (tmp > 0 && students[tmp - 1].a == students[tmp].a) tmp--;
+        // 可以不用map，直接使用一个很长的数组将学号（学号都是数字）索引到students的下标
+        rnk[students[i].id] = tmp + 1;
+        rnkBy[students[i].id] = by[0];
+    }
+    // 要是将 c, m, e 改为数组索引，代码更简洁
+    for (int i = 1; i <=3; i++) {
+        sort(students.begin(), students.end(), cmpAll[i]);
+        for (int j = 0; j < n; j++) {
+            int tmp = j;
+            if (i == 1)
+                while (tmp > 0 && students[tmp - 1].c == students[tmp].c) tmp--;
+            if (i == 2)
+                while (tmp > 0 && students[tmp - 1].m == students[tmp].m) tmp--;
+            if (i == 3)
+                while (tmp > 0 && students[tmp - 1].e == students[tmp].e) tmp--;
+            if (tmp + 1 < rnk[students[j].id]) {
+                rnk[students[j].id] = tmp + 1;
+                rnkBy[students[j].id] = by[i];
+            }
+        }
+    }
+
+    while (m--) {
+        cin >> query;
+        if (rnk.count(query)) {
+            cout << rnk[query] << " " << rnkBy[query] << endl;
+        } else cout << "N/A" << endl;
+        // cout << (rnk.count(query) ? rnk[query] + " " + rnkBy[query] : "N/A") << endl;
+    }
+    return 0;
+}
